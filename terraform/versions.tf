436f734199687cc8635cfe33c7480227420628ab
Denis Rastegaev <laft.rast@gmail.com>
2022-08-27 22:06:32 +0500

    07-terraform-04-teamwork

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -5,6 +5,17 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "<имя бакета>"
+    region     = "ru-central1"
+    key        = "<путь к файлу состояния в бакете>/<имя файла состояния>.tfstate"
+    access_key = "<идентификатор статического ключа>"
+    secret_key = "<секретный ключ>"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
